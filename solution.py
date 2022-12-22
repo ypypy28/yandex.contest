cd4076b481f161bf20715f38713992414c9b0c01
Nikolai Diachkov <ypypy28@gmail.com>
2022-12-22 16:42:43 +0300

    rewrite with iterators

--- solution.py
+++ solution.py
@@ -27,67 +27,75 @@ def print_seats(seats):
     print('\n'.join(buf))
 
 
-n = int(input())
-left, right = [], []
-seats = {
-    "left": [],
-    "right": [],
-}
-free_count = {
-    'left': {
-        "window": {i: deque() for i in range(4)},
-        "aisle": {i: deque() for i in range(4)}
-    },
-    'right': {
-        "window": {i: deque() for i in range(4)},
-        "aisle": {i: deque() for i in range(4)}
+class SeatsIterator:
+    def __init__(self, n_seats, position, seats):
+        self._n_seats = n_seats
+        self._position = position
+        self._seats = seats
+        self.row = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.row > len(self._seats):
+            raise StopIteration
+        seats_ = self._seats[self.row]
+        while self._n_seats > free_from(self._position, seats_):
+            self.row += 1
+            if self.row >= len(self._seats):
+                break
+            seats_ = self._seats[self.row]
+        if self._n_seats > free_from(self._position, seats_):
+            raise StopIteration
+
+        return self.row
+
+
+if __name__ == "__main__":
+    n = int(input())
+    left, right = [], []
+    seats = {
+        "left": [],
+        "right": [],
     }
-}
-for i, _ in enumerate(range(n)):
-    row = input().split('_')
-    seats["left"].append(LeftSeats(*row[0]))
-    seats["right"].append(RightSeats(*row[1][::-1]))
-    free_count["left"]["window"][free_from("window", seats["left"][i])].append(i)
-    free_count["left"]["aisle"][free_from("aisle", seats["left"][i])].append(i)
-    free_count["right"]["window"][free_from("window", seats["right"][i])].append(i)
-    free_count["right"]["aisle"][free_from("aisle", seats["right"][i])].append(i)
+    for i, _ in enumerate(range(n)):
+        row = input().split('_')
+        seats["left"].append(LeftSeats(*row[0]))
+        seats["right"].append(RightSeats(*row[1][::-1]))
 
-m = int(input())
-passengers = []
-for _ in range(m):
-    num, side, position = input().split()
-    num = int(num)
-    sideSeats = LeftSeats if side == "left" else RightSeats
-    i = None
-    for j in range(num, 4):
+    iterators = {
+        "left": {
+            "window": {i: iter(SeatsIterator(i, "window", seats['left'])) for i in range(1, 4)},
+            "aisle": {i: iter(SeatsIterator(i, "aisle", seats['left'])) for i in range(1, 4)},
+        },
+        "right": {
+            "window": {i: iter(SeatsIterator(i, "window", seats['right'])) for i in range(1, 4)},
+            "aisle": {i: iter(SeatsIterator(i, "aisle", seats['right'])) for i in range(1, 4)},
+        }
+    }
+
+    m = int(input())
+    passengers = []
+    for _ in range(m):
+        num, side, position = input().split()
+        num = int(num)
+        sideSeats = LeftSeats if side == "left" else RightSeats
+        i = None
         try:
-            i = free_count[side][position][j].popleft()
-            while free_from(position, seats[side][i]) < num:
-                i = free_count[side][position][j].popleft()
-            break
-        except IndexError:
+            i = next(iterators[side][position][num])
+        except StopIteration:
+            print("Cannot fulfill passengers requirements")
             continue
-    if free_from(position, seats[side][i]) < num:
-        print("Cannot fulfill passengers requirements")
-        continue
 
-    if position == "window":
-        seats[side][i] = sideSeats(*('X' for _ in range(num)), *seats[side][i][num:])
-        positions = ' '.join(sorted([f"{i+1}{field}" for _, field in zip(range(num), seats[side][i]._fields)]))
-        free_count["left"]["window"][free_from("window", seats["left"][i])].appendleft(i)
-        free_count["left"]["aisle"][free_from("aisle", seats["left"][i])].appendleft(i)
-        free_count["right"]["window"][free_from("window", seats["right"][i])].appendleft(i)
-        free_count["right"]["aisle"][free_from("aisle", seats["right"][i])].appendleft(i)
-    else:
-        seats[side][i] = sideSeats(*seats[side][i][:3-num], *('X' for _ in range(num)))
-        positions = ' '.join(sorted([f"{i+1}{field}" for _, field in zip(range(num), seats[side][i]._fields[::-1])]))
-        free_count["left"]["window"][free_from("window", seats["left"][i])].appendleft(i)
-        free_count["left"]["aisle"][free_from("aisle", seats["left"][i])].appendleft(i)
-        free_count["right"]["window"][free_from("window", seats["right"][i])].appendleft(i)
-        free_count["right"]["aisle"][free_from("aisle", seats["right"][i])].appendleft(i)
+        if position == "window":
+            seats[side][i] = sideSeats(*('X' for _ in range(num)), *seats[side][i][num:])
+            positions = ' '.join(sorted([f"{i+1}{field}" for _, field in zip(range(num), seats[side][i]._fields)]))
+        else:
+            seats[side][i] = sideSeats(*seats[side][i][:3-num], *('X' for _ in range(num)))
+            positions = ' '.join(sorted([f"{i+1}{field}" for _, field in zip(range(num), seats[side][i]._fields[::-1])]))
 
-    print(f"Passengers can take seats: {positions}")
+        print(f"Passengers can take seats: {positions}")
 
-    print_seats(seats)
-
-    seats[side][i] = sideSeats(*((ch, '#')[ch == 'X'] for ch in seats[side][i]))
+        print_seats(seats)
+        seats[side][i] = sideSeats(*((ch, '#')[ch == 'X'] for ch in seats[side][i]))
