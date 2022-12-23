f201109faf72dfae82cb3be6813e19497db5da4d
Nikolai Diachkov <ypypy28@gmail.com>
2022-12-23 13:18:54 +0300

    add ability to pass solution scriptname

--- test.sh
+++ test.sh
@@ -1,11 +1,16 @@
 #!/bin/env bash
 set -euo pipefail
+
+solution=solution.py
+if [ $# == 1 ] ; then
+    solution=$1
+fi
 i=0
 filenames=./input*.txt
 for filename in $filenames
 do
     i=$((i+1))
-    cat input$i.txt | python3 solution.py > res$i.txt
+    cat input$i.txt | python3 $solution > res$i.txt
     diff --color=always --unified res$i.txt output$i.txt && echo "TEST $i OK"
 done
 rm res*.txt
