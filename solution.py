59b3db245c78f59883739cca92a29ade283063fb
Nikolai Diachkov <ypypy28@gmail.com>
2022-12-26 20:20:06 +0300

    complete test 6 under 2 seconds

--- solution.py
+++ solution.py
@@ -2,7 +2,6 @@ import math
 
 
 n = int(input())
-
 DIVISOR = 10**9 + 7
 
 
@@ -16,28 +15,16 @@ def combinations(n, k):
 
     rest = 1
     for num in range(sub+1, n+1):
-        rest *= num
+        rest = (rest * num) % DIVISOR
 
-    return rest // math.factorial(k) % DIVISOR
-    # for num in range(sub+1, n+1):
-    #     rest *= num
-    #     rest %= DIVISOR
-
-    # fac = math.factorial(k)
-
-    # if math.gcd(rest, fac) != 1:
-    #     return rest // fac % DIVISOR
-
-    # return (rest % DIVISOR) // pow(fac, DIVISOR-2, DIVISOR)
+    return rest * pow(math.factorial(k), DIVISOR-2, DIVISOR) % DIVISOR
 
 
 def f_inclusion_exclusion(n):
     res = 0
 
     for k in range(n+1):
-        # res += (-1)**k * combinations(n, k) * ((1 << combinations(n-k, 2)) % DIVISOR)  #2**combinations(n-k, 2)
         res += (-1)**k * combinations(n, k) * pow(2, combinations(n-k, 2), DIVISOR)
-        # res += (-1)**k * combinations(n, k) * (2**combinations(n-k, 2))
         res %=DIVISOR
 
     return res
