ed73f3415ad69acf29661eb98da2a54de4b6a7a5
Nikolai Diachkov <ypypy28@gmail.com>
2022-12-26 17:31:14 +0300

    not fast enough

--- solution.py
+++ solution.py
@@ -18,17 +18,26 @@ def combinations(n, k):
     for num in range(sub+1, n+1):
         rest *= num
 
-    return rest  // math.factorial(k) % DIVISOR
+    return rest // math.factorial(k) % DIVISOR
+    # for num in range(sub+1, n+1):
+    #     rest *= num
+    #     rest %= DIVISOR
 
+    # fac = math.factorial(k)
 
-    # return math.factorial(n)//(math.factorial(n-k) * math.factorial(k))
+    # if math.gcd(rest, fac) != 1:
+    #     return rest // fac % DIVISOR
+
+    # return (rest % DIVISOR) // pow(fac, DIVISOR-2, DIVISOR)
 
 
 def f_inclusion_exclusion(n):
     res = 0
 
     for k in range(n+1):
-        res += (-1)**k * combinations(n, k) * 2**combinations(n-k, 2)
+        # res += (-1)**k * combinations(n, k) * ((1 << combinations(n-k, 2)) % DIVISOR)  #2**combinations(n-k, 2)
+        res += (-1)**k * combinations(n, k) * pow(2, combinations(n-k, 2), DIVISOR)
+        # res += (-1)**k * combinations(n, k) * (2**combinations(n-k, 2))
         res %=DIVISOR
 
     return res
