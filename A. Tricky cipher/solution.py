b0c74f221fc2d69aa3e69ce033b394cb0b6bb663
Nikolai Diachkov <ypypy28@gmail.com>
2022-12-29 15:43:51 +0300

    solve A for real

--- A. Tricky cipher/solution.py
+++ A. Tricky cipher/solution.py
@@ -8,9 +8,9 @@ for j in range(n):
     f, i, o, d, m, y = input().split(',')
     stage_1 = len(set(f + i + o))
     stage_2 = sum(sum([int(ch) for ch in num]) for num in (d, m)) * 64
-    stage_3 = (string.ascii_uppercase.index(f[0]) + 1) * 256
+    stage_3 = (string.ascii_uppercase.index(f[0].upper()) + 1) * 256
     code = f"{hex(stage_1 + stage_2 + stage_3).upper()[-3:]:0>3}"
-    if j == last:
-        print(code)
-    else:
+    if j != last:
         print(code, end=' ')
+    else:
+        print(code)
