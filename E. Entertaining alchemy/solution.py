39fb0506de40e0aa4289727847a37d86e5afe350
Nikolai Diachkov <ypypy28@gmail.com>
2022-12-29 17:19:36 +0300

    solve example

--- E. Entertaining alchemy/solution.py
+++ E. Entertaining alchemy/solution.py
@@ -1,41 +1,63 @@
-from collections import Counter, dequeue
+from collections import Counter
+
+
+class CycleError(Exception):
+    ...
 
 
 n = int(input())
-potions = {i: None for i in range(1, n+1)}
+empty_base = {1: 0, 2: 0}
+POTIONS = {1: Counter({1: 1, 2: 0}),
+           2: Counter({1: 0, 2: 1})}
+
+
+def normalize_potion(s: int, prev=None) -> None:
+        if POTIONS[s] is None:
+            raise CycleError
+        if len(POTIONS[s]) == 2:
+            return
+
+        a = POTIONS[s].pop(1)
+        b = POTIONS[s].pop(2)
+
+        for ingredient in POTIONS[s].keys():
+            if prev is not None and ingredient in prev:
+                POTIONS[s] = None
+                raise CycleError
+            if len(POTIONS[ingredient]) > 2:
+                if prev is None:
+                    prev = [s]
+                else:
+                    prev.append(s)
+                normalize_potion(ingredient, prev)
+            a += POTIONS[s][ingredient]*POTIONS[ingredient][1]
+            b += POTIONS[s][ingredient]*POTIONS[ingredient][2]
+        POTIONS[s] = Counter({1: a, 2: b})
+
 
 i = 3
-for _ in range(n-1):
+for _ in range(n-2):
     k, *p = [int(m) for m in input().split()]
-    potions[i] = Counter(p)
+    POTIONS[i] = Counter(p)
+    POTIONS[i].update(empty_base)
+    if i in POTIONS[i]:
+        POTIONS[i] = None
     i += 1
 
 q = int(input())
 
 for _ in range(q):
     x, y, s = [int(m) for m in input().split()]
-    if s == 1:
-        print(1 if x > 0 else 0)
-    elif s == 2:
-        print(1 if x >= 0 else 0)
-    elif s not in potions:
-        print(0)
-    len(potions[s]) == 2:
-        print(1 if potions[s].get(1, 0) <= x and potions[s].get(2, 0) <= y else 0)
+    if s not in POTIONS or POTIONS[s] is None:
+        print(0, end='')
+    elif len(POTIONS[s]) == 2:
+        print(1 if POTIONS[s][1] <= x and POTIONS[s][2] <= y else 0, end='')
     else:
-        a = potions[s].pop(1, 0)
-        b = potions[s].pop(2, 0)
-        visited = set((1, 2))
-        to_visit = set(potions[s].keys())
-        while to_visit:
-            ingredient = to_visit.pop()
-            if ingredient in visited:
-                pass
-            if s in potions[ingredient]:
-                print(0)
-                break
+        try:
+            normalize_potion(s)
+        except CycleError:
+            print(0, end='')
+            continue
 
-
-
-
-
+        print(1 if POTIONS[s][1] <= x and POTIONS[s][2] <= y else 0, end='')
+print()
