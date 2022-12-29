c24ec1e75617e404712d0dcb4225cf6e2e720095
Nikolai Diachkov <ypypy28@gmail.com>
2022-12-29 20:09:57 +0300

    WIP

--- E. Entertaining alchemy/solution.py
+++ E. Entertaining alchemy/solution.py
@@ -17,27 +17,33 @@ def normalize_potion(s: int, prev=None) -> None:
         if len(POTIONS[s]) == 2:
             return
 
+        if prev is None:
+            prev = {s}
+        else:
+            prev.add(s)
+
         a = POTIONS[s].pop(1)
         b = POTIONS[s].pop(2)
 
-        for ingredient in POTIONS[s].keys():
-            if prev is not None and ingredient in prev:
+        for ingredient in POTIONS[s]:
+            if (
+                (ingredient in prev)
+                or (ingredient not in POTIONS)
+                or (POTIONS[ingredient] is None)
+            ):
                 POTIONS[s] = None
                 raise CycleError
-            if len(POTIONS[ingredient]) > 2:
-                if prev is None:
-                    prev = [s]
-                else:
-                    prev.append(s)
+            if len(POTIONS[ingredient]) != 2:
                 normalize_potion(ingredient, prev)
             a += POTIONS[s][ingredient]*POTIONS[ingredient][1]
             b += POTIONS[s][ingredient]*POTIONS[ingredient][2]
         POTIONS[s] = Counter({1: a, 2: b})
+        prev.remove(s)
 
 
 i = 3
 for _ in range(n-2):
-    k, *p = [int(m) for m in input().split()]
+    _, *p = [int(m) for m in input().split()]
     POTIONS[i] = Counter(p)
     POTIONS[i].update(empty_base)
     if i in POTIONS[i]:
