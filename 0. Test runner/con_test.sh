638fdb92a2e6a6f438ed49beb0a80a5248bb35e5
Nikolai Diachkov <ypypy28@gmail.com>
2023-01-05 22:13:30 +0300

    can check task with only input.txt as an input (without stdin)

--- 0. Test runner/con_test.sh
+++ 0. Test runner/con_test.sh
@@ -6,11 +6,12 @@ if [ $# == 1 ] ; then
     solution=$1
 fi
 i=0
-filenames=./input*.txt
+filenames="input*.txt"
 for filename in $filenames
 do
     i=$((i+1))
-    cat input$i.txt | python3 $solution > res$i.txt
-    diff --color=always --unified res$i.txt output$i.txt && echo "TEST $i OK"
+    cp $filename input.txt
+    cat input.txt | python3 $solution > res$i.txt
+    diff --color=always --unified res$i.txt output$i.txt && echo "TEST $i OK" || echo "TEST $i Failed"
+    rm input.txt res$i.txt
 done
-rm res*.txt
