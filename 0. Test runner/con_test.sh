a04cb43489a78d7e11b27940bcbee82af1699b21
Nikolai Diachkov <ypypy28@gmail.com>
2023-01-06 18:12:06 +0300

    add simple timer for each test

--- 0. Test runner/con_test.sh
+++ 0. Test runner/con_test.sh
@@ -1,6 +1,10 @@
 #!/bin/env bash
 set -euo pipefail
 
+seconds_since() {
+    printf $(( `date +%s` - $1 )) 
+}
+
 solution=solution.py
 timeoutvalue=15s
 if [ $# == 1 ] ; then
@@ -12,6 +16,8 @@ for filename in $filenames
 do
     i=$((i+1))
     cp $filename input.txt
-    timeout $timeoutvalue python3 $solution < input.txt > res$i.txt && ( diff --color=always --unified res$i.txt output$i.txt && echo -e "TEST $i \033[32mOK\033[39m" || echo -e "TEST $i \033[31mFailed\033[39m"  ) || echo "TEST $i \033[33mTimeout\033[39m ($timeoutvalue)"
+    start_time=$(date +%s)
+    timeout $timeoutvalue python3 $solution < input.txt > res$i.txt && ( diff --color=always --unified res$i.txt output$i.txt && echo -en "TEST $i \033[32mOK\033[39m" || echo -en "TEST $i \033[31mFailed\033[39m"  ) || echo -en "TEST $i \033[33mTimeout\033[39m ($timeoutvalue)"
+    echo " (took $(seconds_since $start_time) seconds)"
     rm input.txt res$i.txt
 done
