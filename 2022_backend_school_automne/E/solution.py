cb42f1ec8c5ffbdd13fa2163ba590125d325e676
Nikolai Diachkov <ypypy28@gmail.com>
2023-01-06 17:26:10 +0300

    repeat old changes use buffering when read

--- 2022_backend_school_automne/E/solution.py
+++ 2022_backend_school_automne/E/solution.py
@@ -2,48 +2,62 @@ import sys
 import array
 
 
+# CHUNK_SIZE = 24576
+CHUNK_SIZE = 8192
 bad_close_idx = None
 saving_close = None
-# open_stack = [0]*578576
-tmp = [0]*578576
-# open_stack = array.array('L', [0]*578576)
-open_stack = array.array('L', tmp)
-del tmp
+# tmp_stack_appends = 0
+open_stack = array.array('I', [0])*8_550_000
+# open_stack = array.array('I', [0]*3300000)
+# print("SIZE OF STACK AT THE BEGGINING: ", sys.getsizeof(open_stack))
+# open_stack = array.array('I')
 stack_i = -1
-with open("input.txt", mode="rb", buffering=10000) as f:
+with open("input.txt", mode="rb", buffering=CHUNK_SIZE) as f:
     pos = 1
     while True:
-        ch = f.read(1)
-        if not ch:
+        tmp = f.read(CHUNK_SIZE)
+        if not tmp:
             break
 
-        if ch == b"{":  # ord("{") = 123
-            stack_i += 1
-            open_stack[stack_i] = pos
-            # print(f"{ch=} {open_stack=} {bad_close_idx=} {saving_close=}")
-        elif ch == b"}":  # ord("}") = 125
-            if stack_i > -1:
-                stack_i -= 1
-                if not saving_close:
-                    try:
-                        saving_close = pos
-                    except ValueError:
-                        pass
-            else:
-                if not bad_close_idx:
-                    bad_close_idx = pos
-                else:
-                    print(-1)
-                    sys.exit(0)
-            # print(f"{ch=} {open_stack=} {bad_close_idx=} {saving_close=}")
-        pos += 1
-        del ch
+        for i, ch in enumerate(tmp):
+            if ch == 123:  # ord("{") = 123
+                stack_i += 1
+                open_stack[stack_i] = i+pos
+                # if stack_i < len(open_stack):
+                #     open_stack[stack_i] = i+pos
+                # else:
+                #     tmp_stack_appends += 1
+                #     open_stack.append(i+pos)
 
+                # open_stack.append(i+pos)
+                # print(f"{ch=} {open_stack=} {bad_close_idx=} {saving_close=}")
+            elif ch == 125:  # ord("}") = 125
+                # if open_stack:
+                if stack_i > -1:
+                    # open_stack.pop()
+                    stack_i -= 1
+                    if not saving_close:
+                        try:
+                            saving_close = i + pos
+                        except ValueError:
+                            pass
+                else:
+                    if not bad_close_idx:
+                        bad_close_idx = i + pos
+                    else:
+                        print(-1)
+                        sys.exit(0)
+                # print(f"{ch=} {open_stack=} {bad_close_idx=} {saving_close=}")
+        pos += CHUNK_SIZE
+        del tmp
+
+# if len(open_stack) == 1:
 if stack_i == 0:
     if bad_close_idx:
         print(-1)
     else:
         print(open_stack[0])
+# elif bad_close_idx and not open_stack:
 elif bad_close_idx and stack_i < 0:
     if saving_close:
         print(min(bad_close_idx, saving_close))
@@ -51,3 +65,5 @@ elif bad_close_idx and stack_i < 0:
         print(bad_close_idx)
 else:
     print(-1)
+# print("APPENDS", tmp_stack_appends)
+# print("SIZE OF STACK AT THE ENd: ", sys.getsizeof(open_stack))
