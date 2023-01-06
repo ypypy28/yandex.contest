5384f1f4badc37b07ba8787e27ac7b9993c8f392
Nikolai Diachkov <ypypy28@gmail.com>
2023-01-06 17:55:42 +0300

    add color

--- 0. Test runner/con_test.sh
+++ 0. Test runner/con_test.sh
@@ -12,6 +12,6 @@ for filename in $filenames
 do
     i=$((i+1))
     cp $filename input.txt
-    timeout $timeoutvalue python3 $solution < input.txt > res$i.txt && ( diff --color=always --unified res$i.txt output$i.txt && echo "TEST $i OK" || echo "TEST $i Failed"  ) || echo "TEST $i Timeout ($timeoutvalue)"
+    timeout $timeoutvalue python3 $solution < input.txt > res$i.txt && ( diff --color=always --unified res$i.txt output$i.txt && echo -e "TEST $i \033[32mOK\033[39m" || echo -e "TEST $i \033[31mFailed\033[39m"  ) || echo "TEST $i \033[33mTimeout\033[39m ($timeoutvalue)"
     rm input.txt res$i.txt
 done
