e5bfed22d7e6c2b7a16d9dcfa370c35ca0ca9c52
Nikolai Diachkov <ypypy28@yandex.ru>
2023-01-24 23:44:07 +0300

    rewrite with more use of sets

--- prepare_for_an_interview/G. Interesting trip/solution.py
+++ prepare_for_an_interview/G. Interesting trip/solution.py
@@ -3,19 +3,18 @@ from collections import deque
 
 def travel(cur, to_, cities, max_distance):
   visited = set()
+  available_roads = [{i
+            for i in range(len(cities))
+            if (abs(cities[i][0] - cities[j][0]) + abs(cities[i][1] - cities[j][1])) <= max_distance
+            and i != j}
+           for j in range(len(cities))]
   next_variants = deque()
-  next_variants.append((cities[cur], 0))
+  next_variants.append((cur, 0))
   while next_variants:
     cur, roads = next_variants.popleft()
     visited.add(cur)
-    to_visit = {val
-                for val in cities
-                if (
-                    abs(val[0] - cur[0]) + abs(val[1] - cur[1]) <= max_distance
-                    and (val not in visited)
-                )
-             }
-    if cities[to_] in to_visit:
+    to_visit = available_roads[cur] - visited
+    if to_ in to_visit:
       return roads + 1
 
     for next_ in to_visit:
@@ -25,7 +24,7 @@ def travel(cur, to_, cities, max_distance):
 
 
 n = int(input())
-cities = tuple(tuple(int(val) for val in input().split()) for _ in range(n))
+cities =[[int(val) for val in input().split()] for _ in range(n)]
 max_distance = int(input())
 from_, to_ = (int(val)-1 for val in input().split())
 
