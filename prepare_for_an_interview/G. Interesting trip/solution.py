611e92518ff715caee979e48740df0029e2362fe
Nikolai Diachkov <ypypy28@yandex.ru>
2023-01-25 00:50:06 +0300

    decrease memory usage

--- prepare_for_an_interview/G. Interesting trip/solution.py
+++ prepare_for_an_interview/G. Interesting trip/solution.py
@@ -8,12 +8,12 @@ def travel(cur, to_, cities, available_roads):
     while next_variants:
         cur, roads = next_variants.popleft()
         visited.add(cur)
-        to_visit = available_roads[cur] - visited
-        if to_ in to_visit:
+        if to_ in available_roads[cur]:
             return roads + 1
 
-        for next_ in to_visit:
-            next_variants.append((next_, roads+1))
+        for next_ in available_roads[cur]:
+            if next_ not in visited:
+                next_variants.append((next_, roads+1))
     return -1
 
 
@@ -22,8 +22,8 @@ cities = [[int(val) for val in input().split()] for _ in range(n)]
 max_distance = int(input())
 from_, to_ = (int(val)-1 for val in input().split())
 
-available_roads = [set() for _ in range(len(cities))]
-for i in range(len(cities)):
+available_roads = [set() for _ in range(n)]
+for i in range(n):
     for j in range(i):
         if abs(cities[i][0] - cities[j][0]) + abs(cities[i][1] - cities[j][1]) <= max_distance:
             available_roads[i].add(j)
