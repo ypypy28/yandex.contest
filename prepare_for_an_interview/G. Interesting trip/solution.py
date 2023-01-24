3afbd770dd5017ed2754dc257765bbfbd0283896
Nikolai Diachkov <ypypy28@yandex.ru>
2023-01-25 00:26:41 +0300

    more efficient reachability

--- prepare_for_an_interview/G. Interesting trip/solution.py
+++ prepare_for_an_interview/G. Interesting trip/solution.py
@@ -1,31 +1,32 @@
 from collections import deque
 
 
-def travel(cur, to_, cities, max_distance):
-  visited = set()
-  available_roads = [{i
-            for i in range(len(cities))
-            if (abs(cities[i][0] - cities[j][0]) + abs(cities[i][1] - cities[j][1])) <= max_distance
-            and i != j}
-           for j in range(len(cities))]
-  next_variants = deque()
-  next_variants.append((cur, 0))
-  while next_variants:
-    cur, roads = next_variants.popleft()
-    visited.add(cur)
-    to_visit = available_roads[cur] - visited
-    if to_ in to_visit:
-      return roads + 1
+def travel(cur, to_, cities, available_roads):
+    visited = set()
+    next_variants = deque()
+    next_variants.append((cur, 0))
+    while next_variants:
+        cur, roads = next_variants.popleft()
+        visited.add(cur)
+        to_visit = available_roads[cur] - visited
+        if to_ in to_visit:
+            return roads + 1
 
-    for next_ in to_visit:
-      next_variants.append((next_, roads+1))
-
-  return -1
+        for next_ in to_visit:
+            next_variants.append((next_, roads+1))
+    return -1
 
 
 n = int(input())
-cities =[[int(val) for val in input().split()] for _ in range(n)]
+cities = [[int(val) for val in input().split()] for _ in range(n)]
 max_distance = int(input())
 from_, to_ = (int(val)-1 for val in input().split())
 
-print(travel(from_, to_, cities, max_distance))
+available_roads = [set() for _ in range(len(cities))]
+for i in range(len(cities)):
+    for j in range(i):
+        if abs(cities[i][0] - cities[j][0]) + abs(cities[i][1] - cities[j][1]) <= max_distance:
+            available_roads[i].add(j)
+            available_roads[j].add(i)
+
+print(travel(from_, to_, cities, available_roads))
