2a44c564b7e0ad4869c1934a11da77650f24d10b
Nikolai Diachkov <ypypy28@yandex.ru>
2023-02-09 20:48:55 +0300

    pass on all tests

--- 2022_training_backend_automne_winter/A. Andrei and acid/solution.py
+++ 2022_training_backend_automne_winter/A. Andrei and acid/solution.py
@@ -6,31 +6,12 @@ if n == 1:
     print(0)
     sys.exit(0)
 
-repr_num = []
-tmp = sys.stdin.read(1)
-diff = cur = prev = 0
-while tmp != ' ':
-    repr_num.append(tmp)
-    tmp = sys.stdin.read(1)
-prev = int(''.join(repr_num))
-repr_num.clear()
+nums = [int(val) for val in input().split()]
 
-while tmp != '':
-    repr_num.append(tmp)
-    tmp = sys.stdin.read(1)
-    if tmp == ' ':
-        cur = int(''.join(repr_num))
-        repr_num.clear()
-        if cur < prev or (cur != prev and diff != 0):
-            print(-1)
-            sys.exit(0)
-        elif cur > prev:
-            diff = cur - prev
-        prev = cur
-cur = int(''.join(repr_num))
-if cur < prev or (cur != prev and diff != 0):
-    print(-1)
-    sys.exit(0)
-if cur > prev:
-    diff = cur - prev
-print(diff)
+for i in range(n-1):
+    if nums[i] > nums[i+1]:
+      print(-1)
+      sys.exit(0)
+
+print(nums[-1] - nums[0])
+
