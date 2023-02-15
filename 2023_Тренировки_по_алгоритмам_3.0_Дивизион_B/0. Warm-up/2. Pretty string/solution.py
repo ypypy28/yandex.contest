4a0112a8cfbd7febd24805cc0ad9aafc9ba198f9
Nikolai Diachkov <ypypy28@gmail.com>
2023-02-15 09:57:06 +0300

    pass until test 13 (wrang answer)

--- 2023_Тренировки_по_алгоритмам_3.0_Дивизион_B/0. Warm-up/2. Pretty string/solution.py
+++ 2023_Тренировки_по_алгоритмам_3.0_Дивизион_B/0. Warm-up/2. Pretty string/solution.py
@@ -1,19 +1,18 @@
-import sys
-
-
 k = int(input())
 line = input()
 
 end = len(line)
 left = right = skipped = max_seq = 0
-# print(k, line)
+counter = {line[0]: 1}
 while right+1 < end:
     if line[left] == line[right+1]:
         right += 1
+        counter[line[left]] = counter.get(line[left], 0) + 1
     else:
         if skipped < k:
             right += 1
             skipped += 1
+            counter[line[right]] = counter.get(line[right], 0) + 1
         else:
             seq = right - left + 1
             if max_seq < seq:
@@ -22,19 +21,17 @@ while right+1 < end:
             cur = line[left]
             while line[left] == cur:
                 left += 1
+            counter[line[old_left]] -= left - old_left
 
             if left > right:
                 right = left
                 skipped = 0
+                counter.clear()
+                counter[line[left]] = 1
             else:
-                skipped = sum([ch != line[left] for ch in line[left:right+1]])
-
-            # print(f"{old_left=} {left=} {skipped=} {seq=} {right=}")
-            # print(f"{left=} {right=} {skipped=} {line[left:right+1]=} {right-left+1=}")
-    # print(f"{left=} {right=} {skipped=} {line[left:right+1]=} {right-left+1=}")
+                skipped = sum(counter.values()) - counter[line[left]]
 
 last = min(right - left + 1 + k - skipped, end)
-# print(f"{last=}")
 if last > max_seq:
     max_seq = last
 
