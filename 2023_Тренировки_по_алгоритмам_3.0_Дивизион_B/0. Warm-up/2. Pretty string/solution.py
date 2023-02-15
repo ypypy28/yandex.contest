9b1d87446bd32255192343c5dd63d0e93c739f5f
Nikolai Diachkov <ypypy28@gmail.com>
2023-02-15 09:38:04 +0300

    time limit on test 8 (original) again

--- 2023_Тренировки_по_алгоритмам_3.0_Дивизион_B/0. Warm-up/2. Pretty string/solution.py
+++ 2023_Тренировки_по_алгоритмам_3.0_Дивизион_B/0. Warm-up/2. Pretty string/solution.py
@@ -18,12 +18,23 @@ while right+1 < end:
             seq = right - left + 1
             if max_seq < seq:
                 max_seq = seq
-            left += 1 # max(left+1, right-k-1)
-            right = left
-            skipped = 0
+            old_left = left
+            cur = line[left]
+            while line[left] == cur:
+                left += 1
+
+            if left > right:
+                right = left
+                skipped = 0
+            else:
+                skipped = sum([ch != line[left] for ch in line[left:right+1]])
+
+            # print(f"{old_left=} {left=} {skipped=} {seq=} {right=}")
+            # print(f"{left=} {right=} {skipped=} {line[left:right+1]=} {right-left+1=}")
     # print(f"{left=} {right=} {skipped=} {line[left:right+1]=} {right-left+1=}")
 
 last = min(right - left + 1 + k - skipped, end)
+# print(f"{last=}")
 if last > max_seq:
     max_seq = last
 
