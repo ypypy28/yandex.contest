0ed004d23dd3969eeb13fe55454a6fdc6745498e
Nikolai Diachkov <ypypy28@gmail.com>
2023-02-15 14:50:30 +0300

    solve task 6

--- 2023_Тренировки_по_алгоритмам_3.0_Дивизион_B/0. Warm-up/6. Operating systems lite/solution.py
+++ 2023_Тренировки_по_алгоритмам_3.0_Дивизион_B/0. Warm-up/6. Operating systems lite/solution.py
@@ -3,14 +3,15 @@ m, n = [int(input()) for _ in range(2)]
 hd = set()
 for os in range(n):
     a, b = [int(val) for val in input().split()]
-    new_os = range(a, b+1)
     corrupted = []
     for prev_os in hd:
-        if prev_os.start in new_os or (prev_os.stop-1) in new_os:
+        if (prev_os[0] <= a <= prev_os[1]
+            or prev_os[0] <= b <= prev_os[1]
+            or (a <= prev_os[0] and b >= prev_os[1])):
             corrupted.append(prev_os)
     for c in corrupted:
         hd.remove(c)
 
-    hd.add(new_os)
+    hd.add((a, b))
 
 print(len(hd))
