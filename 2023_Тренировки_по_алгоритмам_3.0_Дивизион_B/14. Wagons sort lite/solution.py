92589720ae43a43d3966ba02cbaeb6144e57934e
Nikolai Diachkov <ypypy28@gmail.com>
2023-02-17 19:12:13 +0300

    optimise task 14

--- 2023_Тренировки_по_алгоритмам_3.0_Дивизион_B/14. Wagons sort lite/solution.py
+++ 2023_Тренировки_по_алгоритмам_3.0_Дивизион_B/14. Wagons sort lite/solution.py
@@ -3,29 +3,26 @@ from collections import deque
 
 n = int(input())
 wagons_1 = deque([int(val) for val in input().split()])
-wagons_2 = set()
 
 cur = 1
 deadend = []
 
 while wagons_1:
     if deadend and deadend[-1] == cur:
-        wagons_2.add(deadend.pop())
+        deadend.pop()
         cur += 1
     else:
         wagon = wagons_1.popleft()
         if wagon == cur:
-            wagons_2.add(cur)
             cur += 1
         else:
             deadend.append(wagon)
 
 while deadend:
     if deadend[-1] == cur:
-        wagons_2.add(deadend.pop())
+        deadend.pop()
         cur += 1
     else:
         break
 
 print("YES" if not deadend else "NO")
-# print(f"{cur=} {deadend=} {wagons_2=}")
