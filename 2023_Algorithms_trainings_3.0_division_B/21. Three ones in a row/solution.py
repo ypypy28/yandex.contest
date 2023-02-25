5afad891ccb3f5f928774d2d8c932811ed7e0b20
Nikolai Diachkov <ypypy28@gmail.com>
2023-02-25 11:33:37 +0300

    replace list with array just for fun

--- 2023_Algorithms_trainings_3.0_division_B/21. Three ones in a row/solution.py
+++ 2023_Algorithms_trainings_3.0_division_B/21. Three ones in a row/solution.py
@@ -1,10 +1,12 @@
+from array import array
+
 n = int(input())
-base = [0, 2, 4, 7]  # 13 24
+base = array('I', [0, 2, 4, 7])  # 13 24
 
 if n < 4:
     print(base[n])
 else:
-    dp =[-1]*(n+1)
+    dp = array('I', [0])*(n+1)
     dp[0:4] = base
 
     for i in range(4, n+1):
