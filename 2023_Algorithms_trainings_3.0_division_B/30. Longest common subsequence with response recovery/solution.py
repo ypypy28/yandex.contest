c0b3a91549da26fbe972733d973f7a9ff2dd8505
Nikolai Diachkov <ypypy28@gmail.com>
2023-03-09 17:35:01 +0300

    ML on test 11, task 30

--- 2023_Algorithms_trainings_3.0_division_B/30. Longest common subsequence with response recovery/solution.py
+++ 2023_Algorithms_trainings_3.0_division_B/30. Longest common subsequence with response recovery/solution.py
@@ -3,18 +3,17 @@ seq1 = input().split()
 m = int(input())
 seq2 = input().split()
 
-first = ((0, tuple()), (1, (seq1[0],)))[seq1[0] == seq2[0]]
+first = ((0, tuple()), (1, seq1[0]))[seq1[0] in seq2]
 dp = [[first for _ in range(n)] for _ in range(m)]
 
 for i in range(1, n):
-    if seq1[i] !=  seq2[0]:
+    if seq1[i] != seq2[0]:
         dp[0][i] = dp[0][i-1]
     else:
-        cur = dp[0][i-1][0] + 1, (seq2[0],)
-        dp[0][i] = cur
+        dp[0][i] = dp[0][i-1][0] + 1, (seq2[0],)
 
 for j in range(1, m):
-    for i in range(j, n):
+    for i in range(1, n):
         if seq1[i] != seq2[j]:
             dp[j][i] = max(dp[j-1][i], dp[j][i-1])
         else:
@@ -22,4 +21,5 @@ for j in range(1, m):
             cur = prev[0] + 1, (*prev[1], seq1[i])
             dp[j][i] = cur
 
+# print(*dp)
 print(' '.join(dp[-1][-1][1]))
