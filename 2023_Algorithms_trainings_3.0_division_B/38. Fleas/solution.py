ea780c917271f141f6024aa1f0d5b0d9732ae53f
Nikolai Diachkov <ypypy28@gmail.com>
2023-03-12 22:07:34 +0300

    solve task 38

--- 2023_Algorithms_trainings_3.0_division_B/38. Fleas/solution.py
+++ 2023_Algorithms_trainings_3.0_division_B/38. Fleas/solution.py
@@ -1,5 +1,4 @@
 from collections import deque
-from copy import deepcopy
 
 
 MOVEMENTS = (
@@ -14,57 +13,45 @@ MOVEMENTS = (
 )
 
 
-def calc_shortes_path(
+def bfs(
     start: tuple[int, int],
-    end: tuple[int, int],
     n: int,
     m: int,
     board: [list[list[int]]]
-) -> int:
-    if start == end:
-        return 0
+) -> None:
 
-    brd = deepcopy(board)
     cur = start
-    brd[cur[0]][cur[1]] = 0
+    board[cur[0]][cur[1]] = 0
     q = deque((cur,))
 
     while q:
         cur = q.popleft()
-        # print(f"{start=} {cur=} {end=}", *brd, sep='\n', end='\n\n')
-        new_length = brd[cur[0]][cur[1]] + 1
+        new_length = board[cur[0]][cur[1]] + 1
 
         for next_cell in [(j, i)
                           for dy, dx in MOVEMENTS if (
                               0 < (j:=cur[0]+dy) <= n
                               and 0 < (i:=cur[1]+dx) <= m
                           )]:
-            if brd[next_cell[0]][next_cell[1]] == -1:
-                brd[next_cell[0]][next_cell[1]] = new_length
+            if board[next_cell[0]][next_cell[1]] == -1:
+                board[next_cell[0]][next_cell[1]] = new_length
                 if next_cell == end:
                     q.clear()
                     break
                 q.append(next_cell)
-        # print(f"{q=}", end='\n\n')
-
-    return brd[end[0]][end[1]]
+    return
 
 
 n, m, s, t, q = [int(x) for x in input().split()]
 end = s, t
 
 board = [[-1 for _ in range(m+1)] for _ in range(n+1)]
-
-# fleas = [None]*q
+bfs((s, t), n, m, board)
 
 total = 0
 for k in range(q):
-    flea = tuple(int(x) for x in input().split())
-    # flea = (j, i, [])
-    # fleas[k] = flea
-    # board[j][i] = flea
-    length = calc_shortes_path(flea, end, n, m, board)
-    # print(k, length)
+    j, i = tuple(int(x) for x in input().split())
+    length = board[j][i]
     if length == -1:
         total = -1
         break
