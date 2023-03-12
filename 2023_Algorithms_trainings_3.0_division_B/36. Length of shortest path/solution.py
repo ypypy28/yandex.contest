37ed66bd62246ebaba6b82b6ee1b36c133a9d852
Nikolai Diachkov <ypypy28@gmail.com>
2023-03-12 20:19:22 +0300

    solve task 36

--- 2023_Algorithms_trainings_3.0_division_B/36. Length of shortest path/solution.py
+++ 2023_Algorithms_trainings_3.0_division_B/36. Length of shortest path/solution.py
@@ -5,32 +5,33 @@ def calc_shortest_path(start: int, end: int, all_vertices: list[list[int]]) -> i
     if start == end:
         return 0
 
-    res = -1
-    q = deque(((start, 0), ))
+    all_vertices[start][1] = 0
+    q = deque((start,))
     while q:
-        cur, length = q.popleft()
+        cur = q.popleft()
+        new_length = all_vertices[cur][1] + 1
 
-        for v in vertices[cur]:
-            new_length = length + 1
-            if v == end:
-                res = new_length
-                q.clear()
-                break
-            q.append((v, new_length))
+        for v in all_vertices[cur][0]:
+            if all_vertices[v][1] == -1:
+                all_vertices[v][1] = new_length
+                if v == end:
+                    q.clear()
+                    break
+                q.append(v)
 
-    return res
+    return all_vertices[end][1]
 
 
 n = int(input())
 
-vertices = [[] for _ in range(n+1)]
+vertices = [[[], -1] for _ in range(n+1)]
 
 for i, _ in enumerate(range(n), start=1):
     bs = [int(x) for x in input().split()]
 
     for j, b in enumerate(bs, start=1):
         if b == 1:
-            vertices[i].append(j)
+            vertices[i][0].append(j)
 
 start, end = [int(x) for x in input().split()]
 print(calc_shortest_path(start, end, vertices))
