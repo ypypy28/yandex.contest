e278a89769ca9efd214283f6a964eaa325b1c58e
Nikolai Diachkov <ypypy28@gmail.com>
2023-03-13 17:37:48 +0300

    solve task 40

--- 2023_Algorithms_trainings_3.0_division_B/40. Metro/solution.py
+++ 2023_Algorithms_trainings_3.0_division_B/40. Metro/solution.py
@@ -5,7 +5,7 @@ from math import inf
 n, m = [int(input()) for _ in range(2)]
 
 stations = [[set(), set(), inf] for _ in range(n+1)]
-lines = [None for _ in range(n+1)]
+lines = [None for _ in range(m+1)]
 
 for i in range(1, m+1):
     p, *line = [int(x) for x in input().split()]
@@ -27,11 +27,6 @@ q = deque()
 for line in stations[a][1]:
     q.append((a, line, 0))
 
-# if b in stations[a][1]:
-#     stations[a][2] = 0
-#     transfers = 0
-#     q.clear()
-
 while q:
     st, cur_line, transfer = q.popleft()
 
@@ -49,26 +44,4 @@ while q:
                     stations[s][2] = transfer
                     q.append((s, cur_line, transfer))
 
-    # print(f"{st=} {cur_line=} {s=} {q=}")
-    # print(*(f"{i} {s}" for i,s in enumerate(stations)), sep='\n')
-    # cur_st, prev_line, cur_transfer = q.popleft()
-
-    # new_transfer = cur_transfer+1
-
-
-    # for s in stations[cur_st][0]:
-    #     if s in lines[prev_line]:
-    #         if stations[s][2] > cur_transfer:
-    #             stations[s][2] = cur_transfer
-    #             q.append((s, prev_line, cur_transfer))
-    #     else:
-    #         if stations[s][2] > new_transfer:
-    #             stations[s][2] = new_transfer
-    #             for line in stations[s][1]:
-    #                 q.append((s, line, new_transfer))
-        # print(f"{cur_st=} {prev_line=} {s=} {q=}")
-        # print(*(f"{i} {s}" for i,s in enumerate(stations)), sep='\n')
-
-# print(f"{a=}, {b=}")
-# print(*(f"{i} {s}" for i,s in enumerate(stations)), sep='\n')
 print(-1 if stations[b][2] == inf else stations[b][2])
