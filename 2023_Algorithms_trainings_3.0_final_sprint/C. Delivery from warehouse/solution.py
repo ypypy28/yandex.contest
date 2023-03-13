856371d45ad58e71c81dd5b954e839f639dedad7
Nikolai Diachkov <ypypy28@gmail.com>
2023-03-13 23:21:30 +0300

    WA on test 3, task C

--- 2023_Algorithms_trainings_3.0_final_sprint/C. Delivery from warehouse/solution.py
+++ 2023_Algorithms_trainings_3.0_final_sprint/C. Delivery from warehouse/solution.py
@@ -12,8 +12,8 @@ finished = 0
 for i in range(n):
     a, b = [int(x) for x in input().split()]
 
-    heapq.heappush(c1, (a, b, i))
-    heapq.heappush(c2, (b, a, i))
+    heapq.heappush(c1, (-a, b, i))
+    heapq.heappush(c2, (-b, a, i))
 
 elapsed1 = elapsed2 = 0
 
@@ -24,56 +24,50 @@ while finished != n:
     while c2 and orders[c2[0][2]] is not None:
         heapq.heappop(c2)
 
+    # print(f"{c1=} {c2=} {orders=}")
+
     if c1 and not c2:
         a, b, i = heapq.heappop(c1)
+        a = -a
         orders[i] = "1"
         finished += 1
         elapsed1 += a
     elif c2 and not c1:
         b, a, i = heapq.heappop(c2)
+        b = -b
         orders[i] = "2"
         finished += 1
         elapsed2 += b
     else:
         a1, b1, i1 = heapq.heappop(c1)
+        a1 = -a1
         b2, a2, i2 = heapq.heappop(c2)
+        b2 = -b2
+        finished += 1
 
-
-        while c1 and orders[c1[0][2]] is not None:
-            heapq.heappop(c1)
-        while c2 and orders[c2[0][2]] is not None:
-            heapq.heappop(c2)
-        next1 = c1[0] if c1 else (inf, inf, inf)
-        next2 = c2[0] if c2 else (inf, inf, inf)
-        # print(f"{a1, b1, i1=} {elapsed1=} {next1=}")
-        # print(f"{a2, b2, i2=} {elapsed2=} {next2=}")
-
-        if next1[0] + a1 + elapsed1 < next2[0] + b2 + elapsed2:
-            orders[i1] = "1"
-            finished += 1
-            elapsed1 += a1
-            if i1 != i2:
-                heapq.heappush(c2, (b2, a2, i2))
-        elif next1[2] == next2[2]:
-            if a1+elapsed1 < b2+elapsed2:
-                orders[i1] = "1"
-                finished += 1
-                elapsed1 += a1
+        if a1 + elapsed1 > b2 + elapsed2:
+            if b1 < b2:
+                orders[i1] = "2"
+                elapsed2 += b1
                 if i1 != i2:
-                    heapq.heappush(c2, (b2, a2, i2))
+                    heapq.heappush(c2, (-b2, a2, i2))
             else:
                 orders[i2] = "2"
-                finished += 1
                 elapsed2 += b2
                 if i1 != i2:
-                    heapq.heappush(c1, (a1, b1, i1))
+                    heapq.heappush(c2, (-b1, a1, i1))
         else:
-            orders[i2] = "2"
-            finished += 1
-            elapsed2 += b2
-            if i1 != i2:
-                heapq.heappush(c1, (a1, b1, i1))
-        # print(orders)
+            if a1 < a2:
+                orders[i2] = "1"
+                elapsed1 += a2
+                if i1 != i2:
+                    heapq.heappush(c1, (-a1, b1, i1))
+            else:
+                orders[i1] = "1"
+                elapsed1 += a1
+                if i1 != i2:
+                    heapq.heappush(c1, (-a2, b2, i2))
+
 
 # print(f"{elapsed1=} {elapsed2=}")
 print(' '.join(orders))
