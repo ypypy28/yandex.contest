1138094ff5c0dc1034046c0b55a9869dfceee544
Nikolai Diachkov <ypypy28@gmail.com>
2023-03-14 00:10:30 +0300

    still WA on test 4, task C, but more clear

--- 2023_Algorithms_trainings_3.0_final_sprint/C. Delivery from warehouse/solution.py
+++ 2023_Algorithms_trainings_3.0_final_sprint/C. Delivery from warehouse/solution.py
@@ -1,5 +1,4 @@
 import heapq
-from math import inf
 
 
 n = int(input())
@@ -7,7 +6,6 @@ n = int(input())
 c1 = []
 c2 = []
 orders = [None]*n
-finished = 0
 
 for i in range(n):
     a, b = [int(x) for x in input().split()]
@@ -15,7 +13,7 @@ for i in range(n):
     heapq.heappush(c1, (-a, b, i))
     heapq.heappush(c2, (-b, a, i))
 
-elapsed1 = elapsed2 = 0
+finished = elapsed1 = elapsed2 = 0
 
 while finished != n:
     while c1 and orders[c1[0][2]] is not None:
@@ -24,13 +22,11 @@ while finished != n:
     while c2 and orders[c2[0][2]] is not None:
         heapq.heappop(c2)
 
-    # print(f"{c1=} {c2=} {orders=}")
-
+    finished += 1
     if c1 and not c2:
         a, b, i = heapq.heappop(c1)
         a = -a
-        finished += 1
-        if a < b:
+        if elapsed1 + a < elapsed2 + b:
             orders[i] = "1"
             elapsed1 += a
         else:
@@ -39,19 +35,17 @@ while finished != n:
     elif c2 and not c1:
         b, a, i = heapq.heappop(c2)
         b = -b
-        finished += 1
-        if b < a:
+        if elapsed2 + b < elapsed1 + a:
             orders[i] = "2"
             elapsed2 += b
         else:
             orders[i] = "1"
             elapsed1 += a
     else:
-        a1, b1, i1 = heapq.heappop(c1)
+        a1, b1, i1 = c1[0]
         a1 = -a1
-        b2, a2, i2 = heapq.heappop(c2)
+        b2, a2, i2 = c2[0]
         b2 = -b2
-        finished += 1
 
         v11 = a1 + elapsed1
         v12 = a2 + elapsed1
@@ -62,51 +56,22 @@ while finished != n:
         if min_v == v11:
             orders[i1] = "1"
             elapsed1 += a1
-            if i1 != i2:
-                heapq.heappush(c2, (-b2, a2, i2))
+            heapq.heappop(c1)
 
         elif min_v == v12:
             orders[i2] = "1"
             elapsed1 += a2
-            if i1 != i2:
-                heapq.heappush(c1, (-a1, b1, i1))
+            heapq.heappop(c2)
 
         elif min_v == v21:
             orders[i1] = "2"
             elapsed2 += b1
-            if i1 != i2:
-                heapq.heappush(c2, (-b2, a2, i2))
+            heapq.heappop(c1)
 
-        elif min_v == v22:
+        else:
             orders[i2] = "2"
             elapsed2 += b2
-            if i1 != i2:
-                heapq.heappush(c1, (-a1, b1, i1))
-
-
-        # if a1 + elapsed1 > b2 + elapsed2:
-        #     if b1 < b2:
-        #         orders[i1] = "2"
-        #         elapsed2 += b1
-        #         if i1 != i2:
-        #             heapq.heappush(c2, (-b2, a2, i2))
-        #     else:
-        #         orders[i2] = "2"
-        #         elapsed2 += b2
-        #         if i1 != i2:
-        #             heapq.heappush(c2, (-b1, a1, i1))
-        # else:
-        #     if a1 < a2:
-        #         orders[i2] = "1"
-        #         elapsed1 += a2
-        #         if i1 != i2:
-        #             heapq.heappush(c1, (-a1, b1, i1))
-        #     else:
-        #         orders[i1] = "1"
-        #         elapsed1 += a1
-        #         if i1 != i2:
-        #             heapq.heappush(c1, (-a2, b2, i2))
-
+            heapq.heappop(c2)
 
 # print(f"{elapsed1=} {elapsed2=}")
 print(' '.join(orders))
