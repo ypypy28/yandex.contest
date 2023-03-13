3d64e70d37127fc9a9ca6d870b0349d268acaca9
Nikolai Diachkov <ypypy28@gmail.com>
2023-03-13 18:39:37 +0300

    solve task A

--- 2023_Algorithms_trainings_3.0_final_sprint/A. Underground delivery/solution.py
+++ 2023_Algorithms_trainings_3.0_final_sprint/A. Underground delivery/solution.py
@@ -8,19 +8,28 @@ goods = {}
 
 for _ in range(n):
     op = input().split()
-    # print(op, train, goods, sep='\n')
 
     match op:
         case "add", val, name:
             val = int(val)
-            train.extend([name]*val)
+            train.append([name, val])
             goods[name] = goods.get(name, 0) + val
+
         case "delete", val:
             val = int(val)
-            for _ in range(val):
-                name = train.pop()
-                goods[name] -= 1
+            left = train[-1][1] - val
+            while left < 0:
+                name, deleted = train.pop()
+                goods[name] -= deleted
+                val -= deleted
+                left = train[-1][1] - val
+            train[-1][1] = left
+            goods[train[-1][0]] -= val
+            if left == 0:
+                train.pop()
+
         case "get", name:
             print(goods.get(name, 0))
+
         case _:
             raise NotImplemented
